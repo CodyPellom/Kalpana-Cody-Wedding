4d98a5815c4a3269c99fecc8d1cddf292c251f8e
CodyPellom <paulpellom@gmail.com>
2018-11-27 09:59:25 -0500

    added photo gallery

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    setImageOne();
+  ScrollReveal().reveal('.headers', { delay: 500 });
+  ScrollReveal().reveal('#mainLanderImage', { delay: 500 });
+  ScrollReveal().reveal('.subHeaders', { delay: 500 });
+  ScrollReveal().reveal('.circleImageLarge', { delay: 500 });
+  ScrollReveal().reveal('.squareImage', { delay: 500 });
+  ScrollReveal().reveal('.littleCircleImage', { delay: 500 });
+  ScrollReveal().reveal('.descriptors', { delay: 500 });
+
+ 
+  setImageOne();
 
 
 function setImageOne() {
@@ -21,4 +30,35 @@ function setImageThree() {
   function setImageFive() {
     $('#imageSwap').fadeIn(500).html('<img src="https://i.imgur.com/1ujT5Oi.png" />').delay(2000).fadeOut(500, function () { setImageOne(); });
   }
+
+  
     });
+
+    var slideIndex = 1;
+    showSlides(slideIndex);
+    
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    // Thumbnail image controls
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1} 
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none"; 
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block"; 
+      dots[slideIndex-1].className += " active";
+    }
